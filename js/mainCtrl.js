e8cd5200e3f6e65a4d092b13c36243f3951c1325
marcandy <marcandy.nj@gmail.com>
2016-12-13 15:13:44 -0700

    fix

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -16,5 +16,16 @@ angular.module('quoteBook')
       $scope.qText = '';
       $scope.qAuthor = '';
     }
-    
+    //another way to sole this without having to insert the ng-model text and authout directly to the ng-click addQuote as an obj, would be
+    //$scope.addQuote = function() {
+    // var newQuote = {
+    //  text: $scope.qText,
+    //  author: $scope.qAuthor
+    // }
+    // if(dataService.addData(newQuote) //its checking if that is passing true or false in dataService)
+    //    {
+    //    $scope.qText='';
+    //    $scope.qAuthor='';
+    //    }
+    // }
   })
